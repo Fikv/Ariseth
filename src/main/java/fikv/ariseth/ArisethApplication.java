48f45ccbf3a71bd6fd676cf7b2400503b78ee11e
fikv <jakubfik21@gmail.com>
2025-05-09 01:01:06 +0200

    ariseth init

---
+++ src/main/java/fikv/ariseth/ArisethApplication.java
@@ -0,0 +1,13 @@
+package fikv.ariseth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArisethApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArisethApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fikv/ariseth/ArisethApplicationTests.java
@@ -0,0 +1,13 @@
+package fikv.ariseth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArisethApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
