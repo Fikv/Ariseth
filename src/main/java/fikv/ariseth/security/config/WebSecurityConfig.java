d53e5991d07d794e58a234735b3a5ea2384af40a
fikv <jakubfik21@gmail.com>
2026-01-03 19:49:20 +0100

    feat: basic spring security implementation

--- src/main/java/fikv/ariseth/security/config/WebSecurityConfig.java
+++ src/main/java/fikv/ariseth/security/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,46 +13,35 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
-	@Configuration
-	@EnableWebSecurity
-	public class WebSecurityConfig  {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.authorizeHttpRequests((requests) -> requests.requestMatchers("/", "/home").permitAll().anyRequest().authenticated())
+				.formLogin((form) -> form.loginPage("/login").permitAll())
+				.logout(LogoutConfigurer::permitAll);
 
-		@Bean
-		protected UserDetailsService userDetailsService() {
-			UserDetails user = User.builder()
-								   .username("user")
-								   .password(passwordEncoder().encode("user123"))
-								   .roles("USER")
-								   .build();
-			UserDetails admin = User.builder()
-									.username("admin")
-									.password(passwordEncoder().encode("admin123"))
-									.roles("USER", "ADMIN")
-									.build();
-			return new InMemoryUserDetailsManager(user, admin);
-		}
+		return http.build();
+	}
 
-		@Bean
-		protected PasswordEncoder passwordEncoder() {
-			return new BCryptPasswordEncoder();
-		}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-		@Bean
-		protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-			return http
-					.csrf(AbstractHttpConfigurer::disable)
-					.authorizeHttpRequests(
-							request -> request.requestMatchers("/login").permitAll()
-											  .requestMatchers("/**").authenticated()
-					)
-					.formLogin(Customizer.withDefaults())
-					.logout(config -> config
-							.logoutUrl("/logout")
-							.logoutSuccessUrl("/login"))
-					.build();
-		}
+	@Bean
+	public UserDetailsService userDetailsService() {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		UserDetails user = User.builder()
+							   .username("user")
+							   .password("password")
+							   .roles("USER")
+							   .build();
+
+		return new InMemoryUserDetailsManager(user);
 	}
 
 }
