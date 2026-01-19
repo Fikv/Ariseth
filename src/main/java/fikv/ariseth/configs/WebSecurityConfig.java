b003f03eb9974c2e45ab00c6f530c37363aecc17
fikv <jakubfik21@gmail.com>
2026-01-19 22:05:58 +0100

    feat: minor changes security config

--- src/main/java/fikv/ariseth/configs/WebSecurityConfig.java
+++ src/main/java/fikv/ariseth/configs/WebSecurityConfig.java
@@ -1,11 +1,14 @@
 package fikv.ariseth.configs;
 
+import fikv.ariseth.configs.filters.JwtFilter;
 import fikv.ariseth.services.UserServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,39 +16,50 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-	private final UserServiceImpl userService;
+    private final UserServiceImpl userService;
+    private final JwtFilter jwtFilter;
 
-	public WebSecurityConfig(UserServiceImpl userService) {
-		this.userService = userService;
-	}
-
-	@Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		return http.csrf(AbstractHttpConfigurer::disable)
-				.authorizeHttpRequests(request -> request.anyRequest().authenticated())
-				.formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults())
-				.sessionManagement(customizer ->
-						customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.build();
-
+    public WebSecurityConfig(UserServiceImpl userService, JwtFilter jwtFilter) {
+        this.userService = userService;
+        this.jwtFilter = jwtFilter;
     }
 
-	@Bean PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder(14);
-	}
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+						.requestMatchers("/api/v1/users/register", "/api/v1/users/login").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(customizer ->
+                        customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 
-	@Bean
-	public AuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setUserDetailsService(userService);
-		provider.setPasswordEncoder(passwordEncoder());
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(14);
+    }
 
-		return provider;
-	}
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userService);
+        provider.setPasswordEncoder(passwordEncoder());
+
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 
 }
