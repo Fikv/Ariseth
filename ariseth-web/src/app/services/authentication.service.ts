e06b087ca0c3f900e6c2f649b772dbef96a3ae40
fikv <jakubfik21@gmail.com>
2026-01-30 22:18:11 +0100

    feat: FE auth service template to fill

--- ariseth-web/src/app/services/authentication.service.ts
+++ ariseth-web/src/app/services/authentication.service.ts
@@ -1,9 +1,63 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject, Observable, tap} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private apiUrl = 'http://localhost:8080'; // TODO: change
+  private tokenKey = 'auth_token';
 
-  constructor() { }
+  private isLoggedInSubject = new BehaviorSubject<boolean>(this.hasToken());
+  isLoggedIn$ = this.isLoggedInSubject.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+
+  login(email: string, password: string): Observable<any> {
+    const body = {
+      email,
+      password
+    };
+
+    return this.http.post<any>(`${this.apiUrl}/auth/login`, body).pipe(
+      tap(response => {
+        // TODO: adjust to your backend response
+        if (response?.token) {
+          localStorage.setItem(this.tokenKey, response.token);
+          this.isLoggedInSubject.next(true);
+        }
+      })
+    );
+  }
+
+
+  register(data: {
+    email: string;
+    password: string;
+    name?: string;
+  }): Observable<any> {
+    return this.http.post(`${this.apiUrl}/auth/register`, data);
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.tokenKey);
+    this.isLoggedInSubject.next(false);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  private hasToken(): boolean {
+    return !!localStorage.getItem(this.tokenKey);
+  }
+
+
+  getAuthHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      Authorization: `Bearer ${this.getToken()}`
+    });
+  }
 }
