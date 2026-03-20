79e87210aeffd6895e984a0230f9514f8c101e72
fikv <jakubfik21@gmail.com>
2026-03-20 23:05:41 +0100

    feat: added new table

---
+++ src/main/java/fikv/ariseth/entities/DayPlanner.java
@@ -0,0 +1,14 @@
+package fikv.ariseth.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+//@Entity
+////@Table(name = "Day_planner")
+//public class DayPlanner extends BaseEntity {
+//    @Id()
+//    Long Id;
+//
+//
+//
+//}
