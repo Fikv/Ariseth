cf146e47b04e782afcb3f02c556eada87701e760
fikv <jakubfik21@gmail.com>
2026-03-30 21:37:13 +0200

    feat: tests extensions

--- src/test/java/fikv/ariseth/ArisethApplicationTests.java
+++ src/test/java/fikv/ariseth/ArisethApplicationTests.java
@@ -3,7 +3,7 @@ package fikv.ariseth;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.liquibase.enabled=false")
 class ArisethApplicationTests {
 
 	@Test
