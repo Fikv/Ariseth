31a808a9cba46975af47388a149d1bf3b81b02c6
fikv <jakubfik21@gmail.com>
2026-06-08 22:02:58 +0200

    refactor: sidebar clean

--- ariseth-web/src/app/shared/sidebar/sidebar.component.ts
+++ ariseth-web/src/app/shared/sidebar/sidebar.component.ts
@@ -1,31 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SidebarComponent {
-  readonly menu = [
-    {
-      label: 'Home',
-      icon: 'home',
-      route: '/dashboard'
-    },
-    {
-      label: 'Okabber',
-      icon: 'dayplanner',
-      route: '/okabber'
-    },
-    {
-      label: 'Logo',
-      icon: 'logo',
-      route: '/dashboard'
-    }
-  ];
-}
+export class SidebarComponent {}
