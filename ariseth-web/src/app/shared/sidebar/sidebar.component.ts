a7f973085530b2e9646b0fce42e54de0b5dcf954
fikv <jakubfik21@gmail.com>
2026-06-09 12:25:30 +0200

    feat: FE calendar button added

--- ariseth-web/src/app/shared/sidebar/sidebar.component.ts
+++ ariseth-web/src/app/shared/sidebar/sidebar.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LucideCalendar } from '@lucide/angular';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
+  imports: [LucideCalendar],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
